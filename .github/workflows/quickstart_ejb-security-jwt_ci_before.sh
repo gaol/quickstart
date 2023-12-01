2f76ded2ad1bdc39f079cb0d3172632c6042378a
Lin Gao <aoingl@gmail.com>
2023-12-01 22:37:15 +0800

    Adds test

---
+++ .github/workflows/quickstart_ejb-security-jwt_ci_before.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Start keycloak with required configuration
+docker run -d --rm --name "keycloak" \
+  -p 8180:8080 \
+  -e KEYCLOAK_ADMIN=admin \
+  -e KEYCLOAK_ADMIN_PASSWORD=admin \
+  -v ${GITHUB_WORKSPACE}/quickstarts/ejb-security-jwt/realm:/opt/keycloak/data/import \
+  quay.io/keycloak/keycloak:21.0.0 start-dev --import-realm
